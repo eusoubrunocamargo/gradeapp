43fdcea6e3191386f437fb62a539aa4d23da7bba
Bruno Camargo <eusoubrunocamargo@gmail.com>
2023-05-04 15:31:58 -0300

    iniciando projeto gradeapp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { League_Gothic } from 'next/font/google'
+
+const league_gothic = League_Gothic({subsets: ['latin']})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <main className={league_gothic.className}>
+    <Component {...pageProps} />
+  </main>
+  )
 }
