e0da7dd7ccd856759dd1fdc76bbe517f9a2421b5
Bruno Camargo <eusoubrunocamargo@gmail.com>
2023-05-05 23:25:29 -0300

    signin and signup created

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,12 @@
 import '@/styles/globals.css'
-import { League_Gothic } from 'next/font/google'
+import { League_Gothic, Oswald } from 'next/font/google'
 
 const league_gothic = League_Gothic({subsets: ['latin']})
+const oswald = Oswald({subsets: ['latin']})
 
 export default function App({ Component, pageProps }) {
   return (
-  <main className={league_gothic.className}>
+  <main className={`${league_gothic.className} ${oswald.className}`}>
     <Component {...pageProps} />
   </main>
   )
