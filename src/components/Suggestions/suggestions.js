1b1f264c13bc3fcae08267a72e1b2ad1e676cb84
Bruno Camargo <eusoubrunocamargo@gmail.com>
2023-07-13 23:36:21 -0300

    suggestion

--- src/components/Suggestions/suggestions.js
+++ src/components/Suggestions/suggestions.js
@@ -1,8 +1,28 @@
 import style from '../../styles/Suggestions.module.css'
 import Image from 'next/image'
 import Close from '../../../public/close_neutral.svg'
+import { useState } from 'react'
+import { useAlert } from '@/hooks/useAlert'
+import { supabase } from '../../../supabase'
+import { useUserData } from '@/hooks/useUserData'
+
 
 export default function Suggestions({ openSuggestions, setOpenSuggestions}) {
+
+    const [suggestions, setSuggestions] = useState('');
+    const { handleSuggestion } = useUserData();
+
+    const handleSendSuggestions = () => {
+        if(suggestions === ''){
+            showAlert('Digite sua sugestão', 'fail');
+            return;
+        }
+
+        handleSuggestion(suggestions);
+        setOpenSuggestions(!openSuggestions);
+    }
+
+
 
     return (
         <div className={style.suggestionsFullContainer}>
@@ -11,8 +31,8 @@ export default function Suggestions({ openSuggestions, setOpenSuggestions}) {
                     <Image src={Close} width={25} height={25} alt='close'/>
                 </button>
                 <h1>Sugestões</h1>
-                <input type="text" placeholder="Digite sua sugestão aqui"/>
-                <button>Enviar</button>
+                <input onChange={(e) => setSuggestions(e.target.value)} type="text" placeholder="Digite sua sugestão aqui"/>
+                <button onClick={handleSendSuggestions}>Enviar</button>
             </div>
         </div>
     )
